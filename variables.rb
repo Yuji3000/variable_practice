2e3699201c3eed532b8e67f44fb681d4c889c743
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-07-08 20:48:40 -0500

    finished editing

--- variables.rb
+++ variables.rb
@@ -2,9 +2,16 @@ name = "buddy"
 color = "brown"
 fun = "always"
 age = 1000
-hours_sleeping_a_day = 11
-claws = 2
-running_amount = 2.1
-gallons_drank = 25.2
+hours_sleeping_a_day = "11"
+claw_amount = 2
+running_amount = "2.1"
+gallons_drank = "25.2"
 fur = true
 alive = true
+
+claw_amount # "2"
+claw_amount = 4
+
+puts "The bear is called #{name}"
+puts "The bear has #{claw_amount} claws"
+puts "The bear is #{age} years old"
